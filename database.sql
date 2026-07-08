3d8c15ab5c7eddb024199be53c1c84e3253df374
ShilpaNaik782 <naikshilpa6361@gmail.com>
2026-07-08 21:50:37 +0530

    Create database.sql

---
+++ database.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE student_management;
+
+USE student_management;
+
+CREATE TABLE students (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(100) NOT NULL
+);
